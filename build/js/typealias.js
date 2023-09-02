98038bc4a6ee82a9e2a69121cbb51fcd983a6130
shravan.shan <shravan.shan@nobroker.in>
2023-09-02 18:33:40 +0530

    type alias and literal types

---
+++ build/js/typealias.js
@@ -0,0 +1,13 @@
+"use strict";
+// interface PostName = string | number; // CTE - type aliases not with interface
+let myFirstSelfie = {
+    postId: '123' // can be string or number
+};
+// Literal types
+let steve; // type Steve
+// steve = '123'; // CTE
+let anotherSteve = "Steve";
+// let oneMoreSteve: 'Steve' = "Rachel"; // CTE
+let userName; // using union type, userName can have only one of these three values. kinda like enums
+userName = "Amy";
+// userName = "Rick"; // CTE
---
+++ src/typealias.ts
@@ -0,0 +1,24 @@
+// Type aliases
+type PostId = string | number;
+type RelatedPosts = (string | number)[]
+
+type Post = {
+    postId: PostId,
+    relatedPosts?: RelatedPosts
+}
+
+// interface PostName = string | number; // CTE - type aliases not with interface
+
+let myFirstSelfie: Post = {
+    postId: '123' // can be string or number
+}
+
+// Literal types
+let steve: 'Steve'; // type Steve
+// steve = '123'; // CTE
+let anotherSteve: 'Steve' = "Steve"; 
+// let oneMoreSteve: 'Steve' = "Rachel"; // CTE
+
+let userName: 'Dave' | 'John' | 'Amy'; // using union type, userName can have only one of these three values. kinda like enums
+userName = "Amy";
+// userName = "Rick"; // CTE
