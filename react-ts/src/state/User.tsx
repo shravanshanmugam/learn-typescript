2fbbdca2818e3be6469276086689aa9c738036c4
shravan.shan <shravan.shan@nobroker.in>
2023-09-03 14:20:52 +0530

    useState with type assertion to avoid null check

--- react-ts/src/state/User.tsx
+++ react-ts/src/state/User.tsx
@@ -6,10 +6,15 @@ type AuthUser = {
 }
 export const User = () => {
     const [user, setUser] = React.useState<AuthUser | null>(null);
+    const [anotherUser, setAnotherUser] = React.useState<AuthUser>({} as AuthUser);
     const handleLogin = () => {
         setUser({
             name: "John",
             email: "johndoe@example.com"
+        })
+        setAnotherUser({
+            name: "Jim",
+            email: "jim@example.com"
         })
     }
     const handleLogout = () => {
@@ -19,5 +24,6 @@ export const User = () => {
         <button onClick={handleLogin}>Login</button>
         <button onClick={handleLogout}>Logout</button>
         <div>User name is {user?.name}, email is {user?.email}</div>
+        <div>Another user name is {anotherUser.name}, email is {anotherUser.email}</div>
     </div>
 }
